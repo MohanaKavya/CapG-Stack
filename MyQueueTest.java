c8006c73e17f7f2ef32fd6cd28575f048b244b28
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-12 11:52:49 +0530

    Added : Functionality Added to Dequeue in Queue

--- MyQueueTest.java
+++ MyQueueTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.capgemini.assignment.linkedlist.INode;
 import com.capgemini.assignment.linkedlist.MyNode;
 
 import junit.framework.Assert;
@@ -21,5 +22,24 @@ class MyQueueTest {
 		myQueue.enqueu(thirdNode);
 		myQueue.printQueue();
 		Assert.assertEquals((int) firstNode.getKey(), 56);
+	}
+	@Test
+	public void threeNumbersDeletedFromStackTest() {
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		MyQueue myQueue = new MyQueue();
+		myQueue.enqueu(firstNode);
+		myQueue.enqueu(secondNode);
+		myQueue.enqueu(thirdNode);
+		myQueue.printQueue();
+		INode popedNode = myQueue.dequeue();
+		Assert.assertEquals(firstNode, popedNode);
+		myQueue.printQueue();
+		INode popedNode1 = myQueue.dequeue();
+		Assert.assertEquals(secondNode, popedNode1);
+		myQueue.printQueue();
+		INode popedNode2 = myQueue.dequeue();
+		Assert.assertEquals(thirdNode, popedNode2);
 	}
 }
